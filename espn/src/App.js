5437e191dba504211cd561cad70f285493aba744
AzyaBurgess <Azya.Burgess@cdpipelinedevshop.com>
2022-07-12 16:40:24 +0000

    first commit

---
+++ espn/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './App.css';
+import Login from "./components /Login";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Login></Login>  
+        <p>
+          .
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ espn/src/components /Login.js
@@ -0,0 +1,80 @@
+//Login.js
+import React from "react";
+import Grid from "@mui/material/Grid";
+import { Paper, Avatar, TextField, Checkbox, Button, Typography, Link } from "@mui/material";
+import LockIcon from "@mui/icons-material/Lock";
+import FormControlLabel from "@mui/material/FormControlLabel";
+const Login=()=> {
+    const paperStyle={
+        padding: "20px",
+        height: "100vh",
+        width: 450,
+        margin: "20px auto"
+    }
+    const avatarStyle={
+        backgroundColor:"green"
+    }
+    const btnstyle={
+        margin:"8px 0",
+        borderRadius:"100px"
+    }
+    const textfield={
+        margin:"10px"
+    }
+    return(
+         <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align="center">
+                <img src={require("/workspace/Espn-Sign-in/espn/src/components /images/espn.png")} width="100px"></img>
+  
+                <h2></h2>
+                </Grid>
+        <TextField label="Username or Email Address" placeholder="Enter Username" fullWidth required></TextField>
+        <TextField label="Password (case sensitive)" placeholder="Enter Password" type="password" fullWidth required></TextField>
+        
+            
+            
+            <Button
+                type="Submit"
+                color="primary"
+                variant="contained"
+                style={btnstyle}
+                fullWidth
+                >
+                    Login in
+            </Button>
+            <Typography style={textfield}>
+                    <Link href="#">
+                        Need help logging in?
+                    </Link>
+            </Typography>
+            <Typography style={textfield}>
+                    
+                   Facebook Log in is no longer avaliable. For
+                   help loggin in, please use the link above    
+                  
+                    
+            </Typography>
+            <Button
+                type="Submit"
+                color="primary"
+                variant="outlined"
+                style={btnstyle}
+                fullWidth
+                >
+                    Sign Up
+            </Button>
+        </Paper>
+        </Grid>
+    )
+}
+export default Login;
+
+
+
+
+
+
+
+
+
